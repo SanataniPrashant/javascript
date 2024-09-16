93995c206409c5e0f66411460f87843481b8f081
Prashant Pandey <prashantpandey76867@gmail.com>
2024-09-17 00:00:10 +0530

    first commit

---
+++ firstclass/one.js
@@ -0,0 +1,10 @@
+console.log("hello");
+console.log("This is the first js class");  // cout in console
+
+let num1 = 30;
+let num2 = "pandey";
+
+console.log(typeof(num1)); //  typeof - operator 
+console.log(typeof(num2));
+console.log(typeof(num2));
+console.log(typeof(num2));
