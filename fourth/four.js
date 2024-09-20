d203d2f3296f47e9860d4b77f3b649744b1eaa02
Prashant Pandey <prashantpandey76867@gmail.com>
2024-09-20 12:35:13 +0530

    fhgnf

---
+++ fourth/four.js
@@ -0,0 +1,23 @@
+// function pra(){
+//     var a = parseFloat(window.prompt("Enter Your Salary"));
+//     if(a<350000){
+//         window.alert("you dont have to pay any tax")
+//     }
+//     else if(a>=350000 && a<800000){
+//         var tax = parseFloat((a/100)*10);
+//         window.alert("you have to pay "+ tax +" as tax");
+//     }
+//     else if(a>=800000){
+//         var tax = parseFloat((a/100)*17);
+//         window.alert("you have to pay "+ tax +" as tax");
+//     }
+//     else{
+//         window.alert("Invalid Input");
+//     }
+
+// }
+// pra();
+
+function compare(){
+    window.alert("Ram")
+}
