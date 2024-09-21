2fe463b38be0c4824f6a45db6694c6592a264009
Prashant Pandey <prashantpandey76867@gmail.com>
2024-09-21 12:16:25 +0530

    sg

---
+++ five/five.js
@@ -0,0 +1,14 @@
+function dom(){
+    // let a = document.querySelector("#text");
+    // let a = document.querySelector("#text").textContent;
+    let a = document.querySelector("#text");
+    a.style.color = "red";
+    a.style.fontSize = "28px"
+    // let b =document.querySelector(".heading");
+    let b =document.querySelector(".heading");
+    b.style.color = "yellow";
+    let c = document.querySelector(".heading").textContent;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
