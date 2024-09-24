20a06bf38b2d1d568f320799f63dcbe0c7a6b3a8
Prashant Pandey <prashantpandey76867@gmail.com>
2024-09-24 12:41:05 +0530

    fv

---
+++ sixclass/six.js
@@ -0,0 +1,8 @@
+function ram(){
+    let s = document.querySelector("#img1");
+    s.style.backgroundImage = 'url("https://tse4.mm.bing.net/th?id=OIP.o01pM7cb-KEA7GT-cC7nEgHaE8&pid=Api&P=0&h=180")';
+}
+function ramm(){
+    let s = document.querySelector("#img1");
+    s.style.backgroundImage = 'url("https://tse1.mm.bing.net/th?id=OIP.wwxK07x0Umfnh0l-nrjxjgHaDg&pid=Api&P=0&h=180")';
+}
