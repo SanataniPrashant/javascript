41210b63a0d2fa40eac1442ce41531e73a580c99
Prashant Pandey <prashantpandey76867@gmail.com>
2024-09-25 12:27:32 +0530

    gh

---
+++ seven/seven.js
@@ -0,0 +1,32 @@
+// how to change attribute of any tag with the help of js.
+function ram(){
+    let selectphoto = document.querySelector("#photo");
+    selectphoto.src = "https://images.pexels.com/photos/443446/pexels-photo-443446.jpeg?cs=srgb&dl=daylight-forest-glossy-443446.jpg&fm=jpg";
+
+}
+function ramm(){
+    let selectphoto = document.querySelector("#photo");
+    selectphoto.src = "https://tse1.mm.bing.net/th?id=OIP.vpUUrAb6H7eMCIM4TRzpcAHaFj&pid=Api&P=0&h=180";
+    selectphoto.style.height="900px";
+}
+function rammm(){
+    let s = document.querySelector("ul");
+    s.type="square";
+    s.textContent="fjdjskdb";
+}
+function rest0(){
+    let v = document.querySelector("#next img");
+    v.src = "https://images.pexels.com/photos/1188083/pexels-photo-1188083.png?cs=srgb&dl=sea-dawn-nature-1188083.jpg&fm=jpg";
+}
+function rest1(){
+    let v = document.querySelector("#next img");
+    v.src = "https://wallpaperaccess.com/full/4723250.jpg";
+}
+function rest2(){
+    let v = document.querySelector("#next img");
+    v.src = "https://as2.ftcdn.net/v2/jpg/05/59/20/99/1000_F_559209912_4Qew7SRpZFYR9sg4kqq2YHxBSQeT0VLg.jpg";
+}
+function rest3(){
+    let v = document.querySelector("#next img");
+    v.src = "https://4.bp.blogspot.com/-Imh-wjTJoLQ/UtL0ednyuQI/AAAAAAAAY2g/YPkh9qzvlbU/s1600/240773.jpg";
+}
