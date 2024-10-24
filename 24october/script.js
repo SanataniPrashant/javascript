267ab123f23dd0bcf744725a1b181a8e41e82b02
Prashant Pandey <prashantpandey76867@gmail.com>
2024-10-24 23:41:36 +0530

    ram

--- 24october/script.js
+++ 24october/script.js
@@ -1,13 +1,13 @@
 async function run (){
         let data = await fetch('http://localhost:4000/product');
         let response =await data.json();
-        // console.log(data);
-        // console.log(response);
-        let selecttable = document.querySelector("#table")
+        console.log(data);
+        console.log(response);
+        let selecttable = document.querySelector("#tabledata");
         selecttable.innerHTML = response.map((items)=>`
         <tr>
             <td>${items.id}</td>
-            <td><img src="${items.imgurl}" alt=""></td>
+            <td><img width="100px" src="${items.imageurl}" alt="ram"></td>
             <td>${items.pname}</td>
             <td>${items.bname}</td>
             <td>${items.price}</td>
